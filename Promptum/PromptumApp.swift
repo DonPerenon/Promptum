b6c8babf112ab0f756740bdf558451f9d7194c6b
Viktor <mnogo8555@bk.ru>
2024-03-18 19:15:24 +0300

    Initial Commit

---
+++ Promptum/PromptumApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromptumApp.swift
+//  Promptum
+//
+//  Created by Виктор Иванов on 18.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct PromptumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
